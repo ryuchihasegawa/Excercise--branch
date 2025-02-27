bd8e69fd9ee45cb6ed6b2e9067e90a657122eb45
ryuchihasegawa <145821735+ryuchihasegawa@users.noreply.github.com>
2025-02-27 23:05:46 +0700

    Update index.php
    Menambahkan formulir

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nama = htmlspecialchars($_POST["nama"]);
     $email = htmlspecialchars($_POST["email"]);
+    $telepon = htmlspecialchars($_POST["telepon"]);
+    $alamat = htmlspecialchars($_POST["alamat"]);
+    
     echo "<h2>Data yang Anda masukkan:</h2>";
     echo "<p>Nama: $nama</p>";
     echo "<p>Email: $email</p>";
+    echo "<p>Telepon: $telepon</p>";
+    echo "<p>Alamat: $alamat</p>";
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Formulir Nama anda</title>
+    <title>Formulir Sederhana</title>
 </head>
 <body>
     <h2>Masukkan Data Anda</h2>
@@ -21,6 +26,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         <label for="email">Email:</label>
         <input type="email" name="email" required><br><br>
+        
+        <label for="telepon">Telepon:</label>
+        <input type="text" name="telepon" required><br><br>
+        
+        <label for="alamat">Alamat:</label>
+        <textarea name="alamat" required></textarea><br><br>
         
         <input type="submit" value="Kirim">
     </form>
