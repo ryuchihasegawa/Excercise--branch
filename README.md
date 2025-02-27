2bc2d95dfa09a0b772e00b23914092830044c20e
Gilang Yoga <gilangy538@gmail.com>
2025-02-27 22:34:25 +0700

    Menambahkan Index

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Excercise--branch
+
 Just for excercising
+
+this is the new way
+the way of fire will consume you
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama = htmlspecialchars($_POST["nama"]);
+    $email = htmlspecialchars($_POST["email"]);
+    echo "<h2>Data yang Anda masukkan:</h2>";
+    echo "<p>Nama: $nama</p>";
+    echo "<p>Email: $email</p>";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulir Sederhana</title>
+</head>
+<body>
+    <h2>Masukkan Data Anda</h2>
+    <form method="POST" action="">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" required><br><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br><br>
+        
+        <input type="submit" value="Kirim">
+    </form>
+</body>
+</html>
